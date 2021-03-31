b7209ffe57a16eb5aa2cf6bcd4bfe13b895b22be
Manolo Hoyo Estévez <manuel.hoyo@asesormasmovil.es>
2021-03-31 18:17:48 +0200

    Random entities

--- server/random/random.js
+++ server/random/random.js
@@ -2,10 +2,12 @@ const { bdInit } = require('../bdConf');
 const bcrypt = require('bcrypt-nodejs');
 const bcryptSalt = bcrypt.genSaltSync(10);
 const UserModel = require('../models/user');
+const BuildingModel = require('../models/building');
 const UserLogic = require('../logic/user');
-const users = require('./random-users.json');
+const users = require('./users.json');
+const buildings = require('./buildings.json');
 
-const promises = users.map(
+const userPromises = users.map(
     randomUser => UserLogic.canUseEmail(randomUser.email)
     .then(() => {
         const user = new UserModel();
@@ -19,7 +21,16 @@ const promises = users.map(
     })
 );
 
+const buildingPromises = buildings.map(
+    building => {
+        const newBuilding = new BuildingModel(building);
+        return newBuilding.save();
+    }
+);
+
 bdInit()
-.then(() => Promise.all(promises))
+.then(() => Promise.all(userPromises))
 .then(users => console.log(`Saved ${users.length} users`))
+.then(() => Promise.all(buildingPromises))
+.then(buildings => console.log(`Saved ${buildings.length} buildings`))
 .catch(err => console.error(err));
